ae77f6e52abeb3996c918e2c6ece7d541e4cc5cd
Ben Dominguez <ben@fantasyfootballdatapros.com>
2023-02-26 20:26:06 -0500

    removed test file

--- test.py
+++
@@ -1,9 +0,0 @@
-from model.models.threes import ThreesModel
-
-ThreesModel.run_model(
-    player_name="Jamal Murray",
-    opponent="LAC",
-    bootstrap_samples=100_000,
-    n_simulated_games=10_000,
-    plot=True
-)
